fa3ddf88f7cd7f46ad1f3ce0cc0e31b7e75b5f58
ThatGuyHyperz <69090660+Itz-Hyperz@users.noreply.github.com>
2021-08-03 19:44:56 -0700

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 const figlet = require('figlet');
 const carden = require('carden');
 const ms = require('ms')
+const Importer = require('mysql-import');
 
 module.exports = async(client, con, ready) => {
 
@@ -12,22 +13,30 @@ module.exports = async(client, con, ready) => {
         if(client.config.autoImportSQL) {
         // MySQL Auto Importer Lolz
         try {
-            const importer = new Importer(client.config.mysql);
 
-            // New onProgress method, added in version 5.0!
-            importer.onProgress(progress=>{
-            var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
-            console.log(`${percent}% Completed`);
-            });
-    
-            importer.import('install.sql').then(()=>{
-            var files_imported = importer.getImported();
-            console.log(`${files_imported.length} SQL file(s) imported.`);
-            }).catch(err=>{
-                if(client.config.debugmode) {
-                    console.error(err);
+            con.query(`SELECT * FROM guilds`, async (err, row) => {
+                if(row) {
+                    console.log(chalk.bgBlueBright(`You can set the config variable for auto import to false now if you wish.`))
+                } else {
+                    const importer = new Importer(client.config.database);
+
+                    // New onProgress method, added in version 5.0!
+                    importer.onProgress(progress=>{
+                    var percent = Math.floor(progress.bytes_processed / progress.total_bytes * 10000) / 100;
+                    console.log(`${percent}% Completed`);
+                    });
+            
+                    importer.import('install.sql').then(()=>{
+                    var files_imported = importer.getImported();
+                    console.log(`${files_imported.length} SQL file(s) imported.`);
+                    }).catch(err=>{
+                        if(client.config.debugmode) {
+                            console.error(err);
+                        }
+                    });
                 }
             });
+
         } catch(e) {
             if(client.config.debugmode) {
                 console.log(e)
@@ -35,6 +44,74 @@ module.exports = async(client, con, ready) => {
         }
             
         }
+
+
+        setTimeout(async () => {
+            figlet.text(`HDClient`, { width: '500 '}, async function(err, head) {
+                if (err) return console.log(err);
+                
+                let commandcount = client.config.command_count;
+                let eventcount = client.config.event_count;
+                
+                let frick = `${chalk.white(`Watching `)}${chalk.blue(client.guilds.cache.size)}${chalk.white(' guilds with ')}${chalk.blue(client.users.cache.size)}${chalk.white(' users!')}\n\n${chalk.white(`Client Tag: `)}${chalk.blue(client.user.tag)}\n${chalk.white(`Client ID: `)}${chalk.blue(client.user.id)}\n${chalk.white('Client Age: ')}${chalk.blue(client.user.createdAt.toLocaleString())}\n\n${chalk.white(`Main Prefix: `)}${chalk.blue(client.config.prefix)}${chalk.yellow(' (Default)')}\n${chalk.white(`Commands: `)}${chalk.blue(commandcount)}\n${chalk.white(`Events: `)}${chalk.blue(eventcount)}\n\n${chalk.white(`Created By: `)}${chalk.blue('Hyperz#0001')}\n${chalk.white('Debug Mode: ')}${chalk.yellow(client.config.debugmode)}`;
+    
+                let booter = carden(chalk.blue(head), frick, { margin: 1, content: { borderStyle: 'single', borderColor: "blue", padding: 1}, header: { borderStyle: 'classic', padding: 1}})
+                console.log(booter);
+                console.log(`\n\n    ------ CONSOLE LOGGING BEGINS BELOW ------\n\n`)
+                console.log("Bot started successfully"); // Allows for docker ready event
+            })
+    
+            await client.guilds.cache.forEach(async g => {
+                await con.query(`SELECT * FROM guilds WHERE guildid='${g.id}'`, async(err, row) => {
+                    if(err) throw err
+                    if(row[0]) {
+                        if(row[0].active === 'false') {
+                            await con.query(`UPDATE guilds SET active='true' WHERE guildid='${g.id}'`, async(err, row) => {
+                                if(err) throw err;
+                            });
+                        }
+                    } else {
+                        await con.query(`INSERT INTO guilds (active, guildid, prefix, autobans, autounbans, altprev, altprevtimer, inviteblocker, serverlock) VALUES ('true', '${g.id}', '${client.config.prefix}', 'false', 'false', 'false', '30d', 'false', 'false')`, async (err, row) => {
+                            if(err) throw err;
+                        });
+                    }
+                });
+            });
+    
+            await con.query(`SELECT * FROM guilds`, async (err, row) => {
+                if(err) throw err;
+                await row.forEach(async r => {
+                    let deGuild = await client.guilds.cache.get(r.guildid)
+                    if(deGuild == undefined) {
+                        await con.query(`UPDATE guilds SET active='false' WHERE guildid='${r.guildid}'`, async (err, row) => {
+                            if(err) throw err;
+                        });
+                    } else {
+                        try {
+                            if(deGuild.members.cache.find(client.user.id)) {
+                                return;
+                            } else {
+                                await con.query(`UPDATE guilds SET active='false' WHERE guildid='${r.guildid}'`, async (err, row) => {
+                                    if(err) throw err;
+                                });
+                            }
+                        } catch(e) {
+                            
+                        }
+                    }
+                });
+            });
+    
+            setTimeout(async () => {
+                const channel = client.channels.cache.get(client.config.voicechanneltojoin);
+                if (!channel) return console.error("The voice channel does not exist (change config's voicechanneltojoin)!");
+                channel.join().then(connection => {
+                    console.log("Successfully connected to the voice channel!")
+                }).catch(e => {
+                    console.error(e);
+                });
+            }, 2200);
+        }, 2000)
 
         // Presence Settings
         let presence = [
@@ -45,71 +122,6 @@ module.exports = async(client, con, ready) => {
         ];
 
         changeStatus(client, presence)
-
-        figlet.text(`HDClient`, { width: '500 '}, async function(err, head) {
-            if (err) return console.log(err);
-            
-            let commandcount = client.config.command_count;
-            let eventcount = client.config.event_count;
-            
-            let frick = `${chalk.white(`Watching `)}${chalk.blue(client.guilds.cache.size)}${chalk.white(' guilds with ')}${chalk.blue(client.users.cache.size)}${chalk.white(' users!')}\n\n${chalk.white(`Client Tag: `)}${chalk.blue(client.user.tag)}\n${chalk.white(`Client ID: `)}${chalk.blue(client.user.id)}\n${chalk.white('Client Age: ')}${chalk.blue(client.user.createdAt.toLocaleString())}\n\n${chalk.white(`Main Prefix: `)}${chalk.blue(client.config.prefix)}${chalk.yellow(' (Default)')}\n${chalk.white(`Commands: `)}${chalk.blue(commandcount)}\n${chalk.white(`Events: `)}${chalk.blue(eventcount)}\n\n${chalk.white(`Created By: `)}${chalk.blue('Hyperz#0001')}\n${chalk.white('Debug Mode: ')}${chalk.yellow(client.config.debugmode)}`;
-
-            let booter = carden(chalk.blue(head), frick, { margin: 1, content: { borderStyle: 'single', borderColor: "blue", padding: 1}, header: { borderStyle: 'classic', padding: 1}})
-            console.log(booter);
-            console.log(`\n\n    ------ CONSOLE LOGGING BEGINS BELOW ------\n\n`)
-            console.log("Bot started successfully"); // Allows for docker ready event
-        })
-
-        await client.guilds.cache.forEach(async g => {
-            await con.query(`SELECT * FROM guilds WHERE guildid='${g.id}'`, async(err, row) => {
-                if(err) throw err
-                if(row[0]) {
-                    if(row[0].active === 'false') {
-                        await con.query(`UPDATE guilds SET active='true' WHERE guildid='${g.id}'`, async(err, row) => {
-                            if(err) throw err;
-                        });
-                    }
-                } else {
-                    await con.query(`INSERT INTO guilds (active, guildid, prefix, autobans, autounbans, altprev, altprevtimer, inviteblocker, serverlock) VALUES ('true', '${g.id}', '${client.config.prefix}', 'false', 'false', 'false', '30d', 'false', 'false')`, async (err, row) => {
-                        if(err) throw err;
-                    });
-                }
-            });
-        });
-
-        await con.query(`SELECT * FROM guilds`, async (err, row) => {
-            if(err) throw err;
-            await row.forEach(async r => {
-                let deGuild = await client.guilds.cache.get(r.guildid)
-                if(deGuild == undefined) {
-                    await con.query(`UPDATE guilds SET active='false' WHERE guildid='${r.guildid}'`, async (err, row) => {
-                        if(err) throw err;
-                    });
-                } else {
-                    try {
-                        if(deGuild.members.cache.find(client.user.id)) {
-                            return;
-                        } else {
-                            await con.query(`UPDATE guilds SET active='false' WHERE guildid='${r.guildid}'`, async (err, row) => {
-                                if(err) throw err;
-                            });
-                        }
-                    } catch(e) {
-                        
-                    }
-                }
-            });
-        });
-
-        setTimeout(async () => {
-            const channel = client.channels.cache.get(client.config.voicechanneltojoin);
-            if (!channel) return console.error("The voice channel does not exist (change config's voicechanneltojoin)!");
-            channel.join().then(connection => {
-                console.log("Successfully connected to the voice channel!")
-            }).catch(e => {
-                console.error(e);
-            });
-        }, 2200);
 
     } catch(e) {
         console.log(e)
