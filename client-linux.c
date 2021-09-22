d4fbdfeb0d20e0227ee4225fc8c1d7fac4981792
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-09-22 12:01:15 -0400

    Update client-linux.c

--- client-linux.c
+++ client-linux.c
@@ -5,7 +5,7 @@
 int main() {
   printf("Discord Security Bot\n");
   system("sudo npm install");
-  system("sudo node main.js");
+  system("node main.js");
   printf("License:\n");
   system("cat LICENSE");
   return 0;
